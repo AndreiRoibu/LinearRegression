86b5dafe9ceeb1fe5f5b367b916d67ffea797c0a
Andrei Roibu <aroibu1@gmail.com>
2021-02-25 21:52:53 +0000

    updated the name and description of the setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Basic Numpy',
+    name='Linear Regression',
     version='0.0.1',
-    description='A series of basic Numpy files',
+    description='A series of basic linear regression files',
     license='BSD 3-clause license',
     maintainer='AndreiRoibu',
     maintainer_email='aroibu1@gmail.com',
